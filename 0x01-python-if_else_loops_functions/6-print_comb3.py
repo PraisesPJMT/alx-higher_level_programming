4a90d006b8af2619eba9dfb998c887cb900c6f4d
PraisesPJMT <praisesmusa@outlook.com>
2022-08-24 05:05:20 +0100

    0x01. Python - if/else, loops, functions: Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(10):
+    for j in range(1, 10):
         if i != j:
             print("{}{}, ".format(i, j), end="")
         elif i == j and j == 9:
