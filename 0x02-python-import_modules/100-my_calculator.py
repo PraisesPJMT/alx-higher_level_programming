10acbee9efcef765f32f27575cf08552e9b763c4
PraisesPJMT <praisesmusa@outlook.com>
2022-08-25 22:47:55 +0100

    0x02. Python - import & modules: Task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
     argv = sys.argv
-    if len(argv) == 1:
+    if len(argv) != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         sys.exit(1)
     else:
