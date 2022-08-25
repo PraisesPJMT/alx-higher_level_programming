f6dad5374a053c39e1ff275a7d09747369f69348
PraisesPJMT <praisesmusa@outlook.com>
2022-08-25 21:39:05 +0100

    0x02. Python - import & modules: Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     argv = sys.argv
-    if len(argv) == 0:
+    if len(argv) == 1:
         print('0 arguments.')
     else:
         print('{} arguments'.format(len(argv) - 1))
