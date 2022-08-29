da0484e47f9fcb436f99c96d776a32996a1dfd7d
PraisesPJMT <praisesmusa@outlook.com>
2022-08-29 18:07:38 +0100

    0x03. Python - Data Structures: Lists, Tuples: Task 01

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if len(my_list) > idx or idx < 0:
-        return None
-    else:
-        return my_list[idx]
+    return(my_list[idx] if 0 <= idx < len(my_list) else "None")
+
