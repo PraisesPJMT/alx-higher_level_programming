220ba3259d96d59ff0c086d72685ab5400029c1a
PraisesPJMT <praisesmusa@outlook.com>
2022-08-29 18:43:56 +0100

    0x03. Python - Data Structures: Lists, Tuples: Task 05

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    if my_string is None:
+        return
     my_new_string = ''
     for char in my_string:
         if char is not 'c' and char is not 'C':
